aa65d0b9747ce4006e7c9beedd480d9b3775744b
Nikunj Raghav <nikunjraghav24@gmail.com>
2020-08-14 13:42:27 +0530

    Changed markers radius

--- Demo/script.py
+++ Demo/script.py
@@ -3,14 +3,14 @@ import pandas
 from flask import Flask, render_template
 
 corona_df = pandas.read_csv(
-    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-10-2020.csv")
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-13-2020.csv")
 # now we have to remove NaN values from latitude and longitude cloumn(if there are any)
 # because if there are then program will give error, as marker can not be place for NaN value
 corona_df = corona_df.dropna(subset=['Lat'])
 corona_df = corona_df.dropna(subset=['Long_'])
 
 
-def find_top_confirmed(n=15):
+def find_top_confirmed(n=20):
 
     by_country = corona_df.groupby('Country_Region').sum()[
         ['Confirmed', 'Active', 'Deaths', 'Recovered']]
@@ -23,12 +23,12 @@ data_pairs = [(country, confirmed)
               for country, confirmed in zip(cdf.index, cdf['Confirmed'])]
 
 map1 = folium.Map(location=[28.644800, 77.216721],
-                  zoom_start=4, tiles="Stamen toner")
+                  zoom_start=3, tiles="Stamen toner")
 
 
 def marker_placer(x):
-    folium.Circle(location=[x[0], x[1]], radius=10000,
-                  popup='{}\n Confrimed Cases: {}\n Total Deaths: {}\n Recovered Cases: {}\n Active Cases: {}' .format(x[3], x[2], x[4], x[5], x[6]), color='red', fill=True).add_to(map1)
+    folium.Circle(location=[x[0], x[1]], radius=float(x[2]*0.6),
+                  popup='{}\n Confrimed Cases: {}\nTotal Deaths: {}\nRecovered Cases: {}\nActive Cases: {}' .format(x[3], x[2], x[4], x[5], x[6]), color='red', fill=True).add_to(map1)
 
 
 corona_df[['Lat', 'Long_', 'Confirmed', 'Combined_Key', 'Deaths', 'Recovered', 'Active']].apply(
